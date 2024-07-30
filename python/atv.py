25330d41d7b6325643bea210a785cdada5e63057
lucasCristian07 <lucas.projetoia@gmail.com>
2024-07-30 14:46:55 -0300

    Add files via upload

---
+++ python/atv.py
@@ -0,0 +1,528 @@
+# 
+#atividade1
+# nome = input("Digite seu nome:")
+# apelido = input("Digite seu apelido:")
+# endereço = input("digite seu endereço:")
+# cidpos = input("Digite sua cidade e codigo postal:")
+##chamando inputs e printando
+# print(nome,apelido,endereço,cidpos)
+ 
+#atividade2
+# nome1 = input("digite um nome:")
+# ano = input("digite um ano:")
+#print (nome1,"é um/a valente cavaleir@ nascid@ no ano de", ano , nome1, "acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente", nome1, "kauapoderia salvar os moradores da aldeia")
+ 
+#atividade3
+# cidade = "sao paulo"
+ 
+# print(cidade)
+ 
+# cidade = "rio de janeiro"
+# print(cidade)
+
+#atv 5 
+# texto1 = "ola, seja bem vindo"
+# texto2 = "sua primeira vez aqui?"
+
+# print(texto1,"" + texto2)
+
+# atv 6
+#numero = int(input("digite um numero"))
+# print(f"{numero}, vezes 5 é =, numero * 5)
+
+# 7 
+#nome = str(input("qual e o seu nome?"))
+#ano = int(("em que ano vc nasceu?"))
+#print(f"ola {nome}, voce tera {2024 - ano} anos no final do ano de 2024")
+
+# 8 
+#produto = int(input("qual é o valor do produto?"))
+#desconto - int(input("qual é o valor do desconto"))
+
+#print(f"o novo valor do produto é:{produto -  ((produto/ 100) * desconto)}") 
+
+# 9 
+
+#valorConta = float(input("qual o valor da sua conta"))
+#percentual = int(input{"qual é a porcetagem da gorjeta?"})
+#print(f"o valor da gorjeta e de ", {percentual * (valorConta / 100)})
+#print(f"o valor total da conta sera de {(percentual * (valores / 100)) + valorConta}")
+
+# 10
+
+# taxa = 0.30
+# dolar = 5.51
+# dinheiro = float(input("qual é o valor que voce dejesa converter?"))
+
+# print(f"o valor convertido com a taxa sera de {(dinheiro / dolar) - taxa}")
+
+# # 11
+# capital = float(input("qual o valor inicial?"))
+# juros = float(input("qual a taxa de juros anual"))
+# tempo = int(input("quantos anos?"))
+# print(f"o montante final sera de {capital + (capital * (juros / 100) * tempo)}")
+ 
+
+#  #12
+#  num1 = int(input("digite um numero"))
+#  num2 = int(input("digite outro numero"))
+#  num3 = int(input("digite outro numero"))
+
+# print(f"a media dos numeros é {(num1 + num2 + num3) / 3}")
+
+# #13
+# largura = int(input("qual a largura?"))
+# altura = int(input("qual a altura?"))
+
+# print(f"a area é de {largura * altura} e o perimentro é de {2 * (largura + altura)}")
+
+# #14
+
+# c = float(input("qual é a temperatura atual?"))
+# print(f"converta em fahrenheit { c * 9/5 + 32}")
+
+# #15
+
+# print("trocando um pneu furado")
+# print("1º verifique se o local é seguro")
+# print("2º sinalize o local")
+# print("3º verifique os equipamentos")
+# print("4º erguer o carro")
+# print("5º remover os parafusos")
+# print("6º remover o pneu")
+# print("7º colocar o step")
+# print("8º colocar os parafuso")
+# print("9º apertar os parafuso")
+# print("10º abaixar o carro")
+# print("11º guardar as ferramenta")
+# print("12º remover sinalizaçao")
+# print("pneu trocado")
+
+# #16
+# num1 = int(input("digite um numero "))
+# if num1 < 0:
+#  print(f"{num1 * -1}")
+# else:
+#  print(num1)
+
+#  #17
+
+#  custo =  5.9
+#  nome = str(input("qual o seu nome"))
+
+#  if nome == "jerry"
+#  print(custo)
+
+# else:
+#  porcao = int(input("quantas porçoes foram compradas "))
+# print(f"o valor da total da porçao é de : {porcao * custo}")
+
+
+# #18
+# numero1 = int(input("digite um numero "))
+# if numero1 >= 100 < 1000:
+#  print(f"numero é menor que 1000, obrigado!")
+#  if numero1 >= 10 < 100:
+#   if numero1 < 10
+#   print(F"numero é menor que 1000, menor que 100 e menor que 100 e menor 10, obrigado!")
+#   if numero1 >= 1000
+#    print("obrigado!")
+
+
+# estrutura de repetiçao
+ 
+# while true:
+#     numero = int(input("enrtre com um numero ou digite -1 para parar"))
+ 
+#     if numero == -1;
+#         break
+ 
+#     print(numero ** 2)
+ 
+#     # print("programa encerrado! obrigado")
+ 
+# estrututra de repetiçao simples
+ 
+# while true:
+#     codigo = input("por favor, insira o PIN: ")
+#     if codigo == "1234":
+#         break
+#     print("PIN incorreto. tente novamente")
+ 
+#     print("PIN correto! obrigado")
+ 
+#     print("bem vindo macaco")
+#     while true:
+#         resposta = input("voce quer continuar? (sim/nao):")
+#         if resposta == "nao"
+#         print(okay! ate a proxima!)
+#               break
+             
+# from math import sqrt
+ 
+# print(sqrt(9))
+ 
+# while true:
+# numero = int(input("digite um numero"))
+ 
+# if numero < 0:
+#     print("numero invalido")
+# elif numero > 0:
+#     print(f"A raiz quadadra de {numero} e {sqrt(numero)}")
+# else:
+#     print("programa encerrado")
+#     break
+ 
+# atv40
+# numero = 5
+# print("contando")
+# while true:
+#     print(numero)
+#     numero = numero - 1
+#     if numero > 0:
+#         break
+# print("foi!")
+ 
+# senha = int(input("digite sua senha"))
+# senha2 = int(input("digite novamente"))
+# while true:
+#     if senha =! senha2:
+#         print("acesso negado")
+       
+#         else
+#         print("acesso liberado")
+#         break
+ 
+  # tentativas = 0
+  # while true:
+  #     codigo = input("digite a senha mongou")
+  #    tentativas += 1
+  #    if codigo == "1234":
+  #        sucesso = True
+  #        break
+  #    if tentativas == 3
+  #     sucesso = False
+  #     break
+  # print("senha errado fdp digita essa porra dnv ai")
+  # if sucesso:
+  #     print("acesso liberado")
+  # else:
+  #     print("acesso negado")
+ 
+ 
+#  tentativas = 0
+#  while true:
+#      senha = input("digite a senha")
+ 
+#      if senha == "1234":
+#          sucesso = true
+#          break
+#      if:
+#          print("senha errada")
+#          tentativas += 1
+#          sucesso = false
+#          break
+#      if sucesso
+#   print(f"senha correta vc tentou {tentativas} vezes")
+#  else:
+#  print("acesso negado")
+
+# print("voce esta pronto?")
+# numero = int(input("por favor, digite um numero: "))
+# while numero > 0:
+#     print(numero)
+#     numero -= 1
+# print("agora!")
+
+
+# import re
+
+# print(re.search("[A-Z]", "Senha"))
+# print(re.search("[a-z]", "Senha"))
+# print(re.search("[0-9]", "Senha"))
+
+# import random
+
+# numero_secreto = random.randint(1,100)
+
+# print(numero_secreto)
+
+# atv50
+# soma = 0
+
+# while soma <= 100:
+#     num = int(input("Digite um número: "))
+#     soma += num
+
+# print("A soma total é:", soma)
+
+# import re
+
+# while True:
+#     senha = input("Digite uma senha (mínimo 8 caracteres, com pelo menos uma letra maiúscula, uma minúscula e um número): ")
+    
+#     if len(senha) < 8:
+#         print("Senha deve ter pelo menos 8 caracteres!")
+#         continue
+    
+#     if not re.search("[A-Z]", senha):
+#         print("Senha deve conter pelo menos uma letra maiúscula!")
+#         continue
+    
+#     if not re.search("[a-z]", senha):
+#         print("Senha deve conter pelo menos uma letra minúscula!")
+#         continue
+    
+#     if not re.search("[0-9]", senha):
+#         print("Senha deve conter pelo menos um número!")
+#         continue
+    
+#     print("Senha válida!")
+#     break
+
+
+# 52
+# import random
+
+
+# numero_secreto = random.randint(1, 100)
+
+# print("Tente adivinhar o numero")
+
+# while True:
+#     tentativa = int(input("Digite um número: "))
+#     if tentativa == numero_secreto:
+#         print("Parabéns! Você adivinhou corretamente!")
+#         break
+#     elif tentativa < numero_secreto:
+#         print("O número que eu estou pensando é maior que", tentativa)
+#     else:
+#         print("O número que eu estou pensando é menor que", tentativa)
+
+#estrutura de repetiçao: while: loop alinhado
+
+# while True: #inicia um loop infinito
+#     numero = int(input("por favor, digite um numero: ")) # solicita ai usuario para digitar um numero
+#     if numero == -1: #verifica se o numero digitado é -1
+#         break #se for 1, interrompe o loop
+#     while numero > 0: #contina o loop enquanto o numero for maior que 0
+#         print(numero) #imprime o numero
+#         numero -= 1 #decrementa o numero 
+
+# def mensagem():
+#      print("Essa é a minha funçao")
+     
+
+# def sete_irmaos():
+#      print("lucas")
+#      print("derby")
+#      print("malboro")
+#      print("gift")
+#      print("calton")
+#      print("camel")
+#      print("bah")
+# sete_irmaos()
+
+# def media():
+#     valor1 = print(input("digite um numero"))
+#     valor2 = print(input("digite outro numero"))
+#     valor3 = print(input("digite mais numero"))
+#     media = (valor1 + valor2 + valor3) / 3
+#     print("o resultado é: ", media)
+# media()
+
+# def media(valor1, valor2, valor3):
+#     calculo = (valor1 + valor2 + valor3) / 3
+#     print(calculo)
+#     media(5,10,10)
+
+# def quadrado_hash(tamanho):
+#     for i in range(tamanho):
+#         print("#" * tamanho)
+# quadrado_hash(3)
+
+# def mesaXadrex(tamanho):
+#     for pretu in range(tamanho):
+#         for brancu in range(tamanho):
+#             if (pretu+brancu) % 2 == 0:
+#                 print("1", end=" ")
+#             else:
+#                 print("0", end=" ")
+#         print()
+# mesaXadrex(3)    
+# 
+# def triangulo(tamanho):
+#     for i in range(tamanho):
+#         print(' ' * (tamanho - i - 1) + '#' * (i + 1))       
+# triangulo(5)
+
+# def minha_soma(a, b):
+#     return a + b
+# resultado = minha_soma(4, 6)
+# print("a soma é", resultado)
+
+# def maior_numero(a , b ,c):
+#     if a > b and a > c:
+#         return a
+#     elif b > a and b > c:
+#         return b
+#     else:
+#         return c
+# print(maior_numero(5, 7, 9))  
+          
+# def mesmo_caracter(s: str, n:int , m: int ) -> bool:
+    
+#     if n < 0 or m < 0 or n >= len(s) or m >= len(s):
+#         return False
+#     return s[n] == s[m]
+# print(mesmo_caracter("hello", 0, 0))  
+
+# Inicializa a lista com valores
+# lista = [1, 2, 3, 4, 5]
+# while True:
+#     print("Lista atual:", lista)
+#     indice = int(input("Digite o índice (-1 para sair): "))
+#     if indice == -1:
+#         break
+#     novo_valor = int(input("Digite o novo valor: "))
+#     if indice < 0 or indice >= len(lista):
+#         print("Índice inválido. Tente novamente.")
+#         continue
+#     lista[indice] = novo_valor
+# print("Programa encerrado.")
+
+# numeros = []
+# numeros.append(5)
+# numeros.append(10)
+# numeros.append(3)
+# print(numeros)
+
+
+# num_itens = int(input("Quantos itens você deseja adicionar? "))
+# itens = []
+# i = 0
+# while i < num_itens:
+#     item = input(f"Digite o item {i+1}: ")
+#     itens.append(item)
+#     i += 1
+# print("Lista de itens:")
+# print(*itens, sep='\n')
+
+
+# lista = []
+
+# while True:
+    
+#      escolha = input("Deseja adicionar (A) ou remover (R) um item? ")
+
+#      if escolha.upper() == "A":
+#          if not lista:
+#              lista.append(1)
+#          else:
+#              lista.append(lista[-1] + 1)
+#          print("Lista atual:", lista)
+#      elif escolha.upper() == "R":
+#          if lista:
+#              lista.pop()
+#              print("Lista atual:", lista)
+#          else:
+#              print("Lista vazia. Não há itens para remover.")
+#      else:
+#          print("Escolha inválida. Por favor, escolha A ou R.")
+
+#recebe uma lista como parametro
+# show_tamanhos = [45,44,36,49,40]
+# def mediana(minha_lista: list):
+#     ordenada = sorted(minha_lista)
+#     centro_lista = len(ordenada) // 2 
+#     return ordenada[centro_lista]
+
+# print("a mediaana é", mediana(show_tamanhos))
+# idades = [1,56,34,22,5,77,5]
+# print("a mediana das idades é", mediana(idades))
+
+#retorna uma lista
+# def entrada_numeros():
+#     numeros = []
+#     while True:
+#         entrada_usuarios = input("Por favor, digite um numero inteiro, deixe em branco para sair")
+#         if len(entrada_usuarios) == 0:
+#             break
+#         numeros.append(int(entrada_usuarios))
+#         return numeros
+
+# def media(lista):
+#     return sum(lista) / len(lista)
+# notas = [9, 8, 5, 10, 7, 7, 4, 10, 7, 7, 8, 8, 7]
+# media_notas = media(notas)
+# print(media_notas)
+ 
+#lista
+# for <variavel> em <coleçao>:
+# <bloco>
+# minha_lista = [3,2,4,5,2]
+# for item in minha_lista:
+#     print(item)
+
+# minha_lista = "python"
+
+# for item in minha_lista:
+#   print(f"(item) * ", end="")
+
+#range
+# for i in range(5):(um argumento é o numero total de repetiçao)
+#     print(i)
+
+# for i in range(3,7): (ela retornara um intervalo entre dois numeros)
+#     print(i)
+
+
+# n = int(input("Digite um inteiro positivo: "))
+
+# for i in range(-n, n + 1):
+#         if i != 0:
+#             print(i)
+
+
+# def lista_estrelas(lista):
+#     for num in lista:
+#         print("*" * num)
+# lista_estrelas([3, 5, 2, 1])
+
+# def soma_positivo(lista):
+
+#     return sum(x for x in lista if x > 0)
+# lista = [1, -2, 3, -4, 5]
+# print(soma_positivo(lista))  
+
+def lista_soma(lista1, lista2):
+  """
+  Soma os elementos correspondentes de duas listas.
+
+  Args:
+    lista1: A primeira lista de inteiros.
+    lista2: A segunda lista de inteiros.
+
+  Returns:
+    Uma nova lista com as somas dos elementos correspondentes.
+  """
+
+  # Verifica se as listas têm o mesmo tamanho (opcional)
+  if len(lista1) != len(lista2):
+    raise ValueError("As listas devem ter o mesmo tamanho.")
+
+  # Cria uma lista vazia para armazenar os resultados
+  resultado = []
+
+  # Itera sobre os elementos das listas, somando e adicionando ao resultado
+  for i in range(len(lista1)):
+    soma = lista1[i] + lista2[i]
+    resultado.append(soma)
+
+  return resultado
+
+# Exemplo de uso:
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+resultado = lista_soma(lista_a, lista_b)
+print(resultado)  # Saída: [5, 7, 9]
---
+++ python/index.py
@@ -0,0 +1,30 @@
+print("ola")
+ 
+print("")
+print("")
+print("")
+ 
+print("")
+print("")
+print("")
+ 
+#meu primeiro python
+nome1= input("digite seu nome:")
+print("ola, " + nome1,"!")
+ 
+#string f 
+resultado = 25
+resultado = 30
+print(f"o resultado é {resultado}, e a conta dos dois é {resultado2 + resultado}")
+ 
+nome = "lucas"
+idade = 18
+skill1 = "python"
+nivel1 = "iniciante"
+skill2 = "html"
+nivel2 = "junior"
+skill3 = "javascript"
+nivel3 = "iniciante"
+minimo = 3000
+maximo = "1000000"
+print(f"ola, eu sou {nome} e tenho {idade} anos . minhas habilidadessao {skill1} no nivel, {skill2} no nivel{nivel2} e {skill3} no nivel {nivel3}. procuro um emprego que pague entre {minimo} e {maximo} reais por mes")
